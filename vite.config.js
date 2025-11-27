eddb7e9255fd1258f0d3a5c8b87f0aa894921ce7
Your Name <your.email@example.com>
2025-11-27 10:39:58 +0100

    fix path

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: "/leonorasherifi.github.io/"
+  base: "/3D-Project/"
 })
